7b25aaa4eb8f2b1e028c97e307b91809b508cd6c
Narahari-Sindhu <129727437+Narahari-Sindhu@users.noreply.github.com>
2024-01-09 20:20:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+var settingsmenu= document.querySelector(".settings-menu")
+var darkBtn=document.getElementById("dark-btn");
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
+}
+darkBtn.onclick=function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("bark-theme");
+
+    if(localStorage.getItem("theme")=="light"){
+        localStorage.setItem("theme" ,"dark");
+    }
+    else{
+        localStorage.setItem("theme" ,"light");
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+if(localStorage.getItem("theme")=="light"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("bark-theme");
+}
+else if(localStorage.getItem("theme")=="dark"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("bark-theme");
+}
+else{
+    localStorage.setItem("theme" ,"light");
+}
+
+
+
+
+localStorage.setItem("theme" , "dark");
+localStorage.getItem("theme");
